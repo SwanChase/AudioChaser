e696480b7f832e1a11657c9a9c54d38e0d57adc6
The1BlackSwan <swancd@gmail.com>
2020-09-30 07:25:30 +0200

    1st push
    1st prototype

---
+++ BeepBeepBoop SGD/Assets/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static MusicManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume(string _mixerName, float _segVolume)
+    {
+
+    }
+}
---
+++ BeepBeepBoop SGD/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float maxRadius = 100f;
+    public float collectRadius = 5f;
+    public float speed = 5f;
+
+    public Transform target;
+    public GameObject child;
+
+    private MeshRenderer meshR;
+
+    void Start()
+    {
+        target = Player.instance.transform;
+        meshR = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        // Get the distance to the player
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= maxRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, -1 * speed * Time.deltaTime);
+        }
+
+        if(distance <= collectRadius)
+        {
+            Tagged();
+        }
+        
+
+    }
+
+    private void Tagged()
+    {
+        meshR.material.color = Color.yellow;
+        child.SetActive(false);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, collectRadius);
+    }
+
+}
